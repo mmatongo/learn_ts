2771df391e21f9dd992f9d498b9de65eb272849d
Daniel M. Matongo <m_matongo@icloud.com>
2022-02-03 13:10:43 +0000

    add boolean type

--- types/types.ts
+++ types/types.ts
@@ -1,17 +1,20 @@
 interface Person {
   name: string;
   age: number;
+  male: boolean;
 }
 
 class NewPerson {
   name: string;
   age: number;
+  male: boolean;
 
-  constructor(name: string, age: number) {
+  constructor(name: string, age: number, male: boolean) {
     this.name = name;
     this.age = age;
+    this.male = male;
   }
 }
 
-const newPerson: Person = new NewPerson('Daniel', 23);
+const newPerson: Person = new NewPerson('Daniel', 23, true);
 console.log(newPerson);
