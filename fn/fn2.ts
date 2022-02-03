3392b51058da8a760b72b98c2c017628d9e8f4ae
Daniel M. Matongo <m_matongo@icloud.com>
2022-02-03 13:21:01 +0000

    add new fn test

---
+++ fn/fn2.ts
@@ -0,0 +1,7 @@
+const greet = (name: string) => {
+  console.log(`Hello, ${name}`);
+};
+
+greet(6) // you don't really need an interface - this will throw an err
+greet('Daniel') // This is just fin
+
