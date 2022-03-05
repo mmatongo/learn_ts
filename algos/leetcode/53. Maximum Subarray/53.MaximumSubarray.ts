e328d51fe59153e9f84cd8432050095d991bab56
Daniel M. Matongo <m_matongo@icloud.com>
2022-03-05 11:05:25 +0200

    #53

---
+++ algos/leetcode/53. Maximum Subarray/53.MaximumSubarray.ts
@@ -0,0 +1,10 @@
+function maxSubArray(nums: number[]): number {
+  let max = nums[0];
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (sum > max) max = sum;
+    if (sum < 0) sum = 0;
+  }
+  return max;
+};
