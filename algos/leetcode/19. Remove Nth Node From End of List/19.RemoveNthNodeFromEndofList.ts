cef6eae7a09d53838f0641151b0efd1dd8ad64f1
Daniel M. Matongo <m_matongo@icloud.com>
2022-03-05 11:00:27 +0200

    #19

---
+++ algos/leetcode/19. Remove Nth Node From End of List/19.RemoveNthNodeFromEndofList.ts
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  let dummy = new ListNode(0);
+  dummy.next = head;
+  let slow = dummy;
+  let fast = dummy;
+  while (n > 0) {
+    fast = fast.next;
+    n--;
+  }
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  slow.next = slow.next.next;
+  return dummy.next;
+};
