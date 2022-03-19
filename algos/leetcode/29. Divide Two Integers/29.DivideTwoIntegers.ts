c27b2ffeafd9d02d27cf24325938b4972412d95f
Daniel M. Matongo <m_matongo@icloud.com>
2022-03-19 21:18:09 +0200

    #29 refactor

--- algos/leetcode/29. Divide Two Integers/29.DivideTwoIntegers.ts
+++ algos/leetcode/29. Divide Two Integers/29.DivideTwoIntegers.ts
@@ -1,44 +1,19 @@
 function divide(dividend: number, divisor: number): number {
-  let symbol = 1;
+  let quotient: number = 0;
+  let isNegative: boolean = false;
+  const edge: number = Math.pow(2, 31);
   if (dividend < 0 && divisor < 0) {
-    dividend = 0 - dividend;
-    divisor = 0 - divisor;
-  } else if (dividend < 0) {
-    symbol = -1;
-    dividend = 0 - dividend;
-  } else if (divisor < 0) {
-    symbol = -1;
-    divisor = 0 - divisor;
+    isNegative = false;
+  } else if (dividend < 0 || divisor < 0) {
+    isNegative = true;
   }
-  let sum = 0;
-  let ratio = 1;
-  let preratio = 0;
-  let predivisor = divisor;
-  let multidivisor = divisor;
-  let i = 0;
-  while (true) {
-    if ((sum + multidivisor) > dividend) {
-      if (ratio === 1) {
-        break;
-      }
-      i += preratio;
-      sum += predivisor
-      preratio = 1;
-      ratio = 1;
-      predivisor = divisor;
-      multidivisor = divisor;
-    } else if ((sum + multidivisor) === dividend) {
-      i += preratio;
-      break;
-    } else {
-      preratio = ratio;
-      ratio += ratio;
-      predivisor = multidivisor;
-      multidivisor += multidivisor;
-    }
+
+  dividend = Math.abs(dividend);
+  divisor = Math.abs(divisor);
+
+  quotient = Math.floor(dividend / divisor);
+  if (quotient > edge - 1) {
+    return isNegative ? -edge : edge - 1;
   }
-  if (symbol > 0 && i > 2147483647) {
-    return 2147483647;
-  }
-  return symbol > 0 ? i : 0 - i;
-};
+  return isNegative ? -quotient : quotient;
+}
