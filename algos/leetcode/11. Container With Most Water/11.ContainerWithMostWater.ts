58bb4cb1afd65a970ae94a7266b946ab32e260b2
Daniel M. Matongo <m_matongo@icloud.com>
2022-03-22 23:41:48 +0200

    #11

---
+++ algos/leetcode/11. Container With Most Water/11.ContainerWithMostWater.ts
@@ -0,0 +1,12 @@
+function maxArea(height: number[]): number {
+  let max = 0;
+  let left = 0;
+  let right = height.length - 1;
+  while (left < right) {
+    const area = Math.min(height[left], height[right]) * (right - left);
+    if (area > max) max = area;
+    if (height[left] < height[right]) left++;
+    else right--;
+  }
+  return max;
+};
